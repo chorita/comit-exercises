e8d28615fa864567da96947cb1fb0e8fa0dbfd31
Carlos Horita <ceduardohorita@gmail.com>
2020-09-22 18:48:49 -0500

    Added Exercise 19

---
+++ Exercise 19-JS/index.js
@@ -0,0 +1,25 @@
+const monthOfBirth = 'June'
+const firstName ='Carlos'
+const lastName ='Horita'
+let city;
+city ='Winnipeg';
+let age;
+age ='35'
+let nextCity;
+nextCity ='Winnipeg'
+
+console.log('monthOfBirth:June, monthOfBirth');
+console.log('firstName:Carlos, firstName');
+console.log('lastName:Horita, lastName');
+console.log('city:Winnipeg, city');
+console.log('age:35, age');
+console.log('nextCity:city, nextCity');
+
+alert(monthOfBirth)
+alert(firstName)
+alert(lastName)
+alert(city)
+alert(age)
+alert(nextCity)
+alert(`my name is ${firstName + lastName}`);
+
