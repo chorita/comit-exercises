5452b024715a20e18815b422e9c3d729dca298a7
Carlos Horita <ceduardohorita@gmail.com>
2020-09-23 22:30:13 -0500

    Added lesson 20

---
+++ Exercise 20-JS/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let age="100";
+console.log(Number(age));
+
+let firstName=true;
+firstName = String("Carlos");
+console.log(firstName);
+
+let a = Boolean('You have completed this course yet');
+console.log(a);
+
+let finalscore=null;
+console.log(finalscore);
+
+let winningNumbers;
+console.log(winningNumbers);
+
+
