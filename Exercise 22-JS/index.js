3d917d88df2ed635c886ca00d5e3d4ea4ddbc22f
Carlos Horita <ceduardohorita@gmail.com>
2020-09-25 22:22:06 -0500

    Added Exercise 22

---
+++ Exercise 22-JS/index.js
@@ -0,0 +1,15 @@
+"use strict";
+alert("Welcome, Enter two numbers to see if the first is greater than the second");
+
+let firstNumber=prompt("Enter first number");
+let secondNumber =prompt("Enter second number");
+
+if (firstNumber > secondNumber) {
+    alert("That is greater");
+} 
+else if (firstNumber < secondNumber) {
+    alert("That its less than");
+}
+else if (firstNumber = secondNumber) {
+    alert("That its the same");
+};
