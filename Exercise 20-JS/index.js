c3402607ae77f4a4fd0293d7d4c72f8796d2999a
Carlos Horita <ceduardohorita@gmail.com>
2020-09-25 21:21:05 -0500

    Fixed Boolean

--- Exercise 20-JS/index.js
+++ Exercise 20-JS/index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 let age="100";
 console.log(Number(age));
@@ -7,8 +7,8 @@ let firstName=true;
 firstName = String("Carlos");
 console.log(firstName);
 
-let a = Boolean('You have completed this course yet');
-console.log(a);
+let courseCompleted=false
+console.log(courseCompleted);
 
 let finalscore=null;
 console.log(finalscore);
