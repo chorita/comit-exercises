1d175adc12689b9bd3806ef5498a948f65250b5c
Carlos Horita <ceduardohorita@gmail.com>
2020-10-01 19:38:21 -0500

    fixing number variable

--- Exercise 21-JS/index.js
+++ Exercise 21-JS/index.js
@@ -1,5 +1,5 @@
 "use strict"
 alert("Welcome, Enter two numbers to see if the first is greater than the second");
-let firstNumber=prompt("Enter First Number");
-let secondNumber=prompt("Enter Second Number");
+let firstNumber=+prompt(Number);
+let secondNumber=+prompt(Number);
 let result = confirm(firstNumber > secondNumber);
