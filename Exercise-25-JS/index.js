5fdd56d975ff368c9ec596945fcd0eb4b5dfa806
Carlos Horita <ceduardohorita@gmail.com>
2020-10-04 15:03:03 -0500

    Fixing Switch

--- Exercise-25-JS/index.js
+++ Exercise-25-JS/index.js
@@ -1,66 +1,66 @@
 "use strict";
+if (weekdayNumber == 1) {
+    console.log("Today is Monday");
+   } 
+   else if
+   (weekdayNumber == 2) {
+    console.log("Today is Tuesday");
+   }
+   else if 
+    (weekdayNumber == 3) {
+   console.log("Today is Wednesday");
+   } 
+   else if (weekdayNumber == 4) {
+   console.log("Today is Thursday");
+   } 
+   else if (weekdayNumber == 5) {
+   console.log("Today is Friday");
+   }
+   else if (weekdayNumber == 6) {
+   console.log("Today is Saturday");
+   } 
+   else if (weekdayNumber == 7) {
+   console.log("Today is Sunday");
+   } 
+   else if (weekdayNumber << 8) {
+   console.log("error message: Error: Number not between 1-7")
+   }
+
 
  let weekdayNumber = +prompt("Choose a number 1 to 7", " ");
 
- if (weekdayNumber == 1) {
-     console.log("Today is Monday");
-    } 
-    else if
-    (weekdayNumber == 2) {
-     console.log("Today is Tuesday");
-    }
-    else if 
-     (weekdayNumber == 3) {
-    console.log("Today is Wednesday");
-    } 
-    else if (weekdayNumber == 4) {
-    console.log("Today is Thursday");
-    } 
-    else if (weekdayNumber == 5) {
-    console.log("Today is Friday");
-    }
-    else if (weekdayNumber == 6) {
-    console.log("Today is Saturday");
-    } 
-    else if (weekdayNumber == 7) {
-    console.log("Today is Sunday");
-    } 
-    else if (weekdayNumber << 8) {
-    console.log("error message: Error: Number not between 1-7")
-    }
+ switch (weekdayNumber) {
+        
+    case 1: 
+        alert("Today is Monday");
+        break;
 
-    switch (weekdayNumber) {
-        case 1: {
-            let message=("Today is Monday");
-            break;
-        }
-        case 2: {
-            let message=("Today is Tuesday");
-            break;
-        }
-        case 3: {
-            let message=("Today is Wednesday");
-            break;
-        }
-        case 4: {
-            let message=("Today is Thursday");
-            break;
-        }
-        case 5: {
-            let message=("Today is Friday");
-            break;
-        }
-        case 6: {
-            let message=("Today is Saturday");
-            break;
-        }
-        case 7: {
-                let message=( "Today is Sunday" );
-                break;
-        }
-        default:
-            let message=("error message: Error: Number not between 1-7");
-            break;
-
+    case 2: 
+        alert("Today is Tuesday");
+        break;
+    
+    case 3: 
+        alert("Today is Wednesday");
+        break;
+    
+    case 4: 
+        alert("Today is Thursday");
+        break;
+    
+    case 5: 
+        alert("Today is Friday");
+        break;
+    
+    case 6: 
+        alert("Today is Saturday");
+        break;
+    
+    case 7: 
+        alert("Today is Sunday");
+        break;
+    
+    default:
+        alert("error message: Error: Number not between 1-7");
+        break;
     }
-    console.log(weekdayNumber + {message});
+console.log(weekdayNumber);
